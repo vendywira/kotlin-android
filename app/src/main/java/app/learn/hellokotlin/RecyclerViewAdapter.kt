f5f43f2421a3b2580af9456047e3327ed4cd7f1c
I Wayan Vendy Wiranatha <vendywira@gmail.com>
2018-10-16 21:24:20 +0700

    refactor code to use kotlin android extension

--- app/src/main/java/app/learn/hellokotlin/RecyclerViewAdapter.kt
+++ app/src/main/java/app/learn/hellokotlin/RecyclerViewAdapter.kt
@@ -7,7 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.squareup.picasso.Picasso
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_list.*
+import kotlinx.android.synthetic.main.item_list.view.*
+import kotlinx.android.synthetic.main.notification_template_custom_big.view.*
 
 class RecyclerViewAdapter(
         private val context: Context,
@@ -16,24 +21,25 @@ class RecyclerViewAdapter(
     : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-            ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_list, parent, false))
+            ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_list, parent, false), null)
 
     override fun getItemCount(): Int = items.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bindItem(items[position], listener)
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-
-        private val name = view.findViewById<TextView>(R.id.name)
-        private val image = view.findViewById<ImageView>(R.id.image)
+    class ViewHolder(view: View, override val containerView: View?) : RecyclerView.ViewHolder(view), LayoutContainer {
 
         fun bindItem(items : Item, listener: (Item) -> Unit) {
-            name.text = items.name
-            items.image?.let { Picasso.get().load(it).into(image) }
+//            name.text = items.name
+//            items.image?.let { Picasso.get().load(it).into(image) }
+
+            itemView.name.text = items.name
+            Glide.with(itemView.context).load(items.image).into(itemView.image)
 
             itemView.setOnClickListener {
                 listener(items)
             }
+
         }
     }
 
